2534053305c44e52152cfaaa663490def499b6c4
Sollace <sollacea@gmail.com>
2019-01-26 10:21:17 +0200

    Fixed crash due to undefined classes when MineLP is not present

---
+++ src/main/java/com/minelittlepony/util/render/Vertex.java
@@ -0,0 +1,19 @@
+package com.minelittlepony.util.render;
+
+import net.minecraft.client.model.PositionTextureVertex;
+
+public class Vertex extends PositionTextureVertex {
+
+    public Vertex(float x, float y, float z, float texX, float texY) {
+        super(x, y, z, texX, texY);
+    }
+
+    private Vertex(Vertex old, float texX, float texY) {
+        super(old, texX, texY);
+    }
+
+    @Override
+    public Vertex setTexturePosition(float texX, float texY) {
+        return new Vertex(this, texX, texY);
+    }
+}
