7ec3f39a298add6cdca5ea6de8015ce7f130618d
Sollace <sollacea@gmail.com>
2019-03-04 14:51:04 +0200

    Added a grass-like overlay to chitin blocks

--- src/main/java/com/minelittlepony/unicopia/block/BlockChitin.java
+++ src/main/java/com/minelittlepony/unicopia/block/BlockChitin.java
@@ -10,15 +10,22 @@ import com.minelittlepony.unicopia.player.PlayerSpeciesList;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockChitin extends Block {
+
+    public static final PropertyEnum<Covering> COVERING = PropertyEnum.create("covering", Covering.class);
 
     public BlockChitin(String domain, String name) {
         super(UMaterials.hive);
@@ -28,6 +35,7 @@ public class BlockChitin extends Block {
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
         setHardness(50);
         setResistance(2000);
+        setDefaultState(blockState.getBaseState().withProperty(COVERING, Covering.UNCOVERED));
     }
 
     @Override
@@ -66,4 +74,42 @@ public class BlockChitin extends Block {
         return hardness;
     }
 
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return 0;
+    }
+
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+        IBlockState s = world.getBlockState(pos.up());
+        Block block = s.getBlock();
+
+        boolean snowy = block == Blocks.SNOW || block == Blocks.SNOW_LAYER;
+        boolean solid = (s.isFullBlock() && s.isFullCube()) || s.isSideSolid(world, pos.up(), EnumFacing.DOWN);
+
+        return state.withProperty(COVERING, snowy ? Covering.SNOW_COVERED : solid ? Covering.COVERED : Covering.UNCOVERED);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, COVERING);
+    }
+
+    public static enum Covering implements IStringSerializable {
+        COVERED,
+        UNCOVERED,
+        SNOW_COVERED;
+
+        @Override
+        public String toString() {
+            return getName();
+        }
+
+        @Override
+        public String getName() {
+            return name().toLowerCase();
+        }
+
+    }
 }
