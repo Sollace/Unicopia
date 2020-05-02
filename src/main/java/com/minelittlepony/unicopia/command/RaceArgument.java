bbfebe009a2fa2a2dec8fa3b7afca922fa6a3241
Sollace <sollacea@gmail.com>
2020-05-02 18:51:19 +0200

    Fixed autocomplete for race arguments

--- src/main/java/com/minelittlepony/unicopia/command/RaceArgument.java
+++ src/main/java/com/minelittlepony/unicopia/command/RaceArgument.java
@@ -2,12 +2,16 @@ package com.minelittlepony.unicopia.command;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import com.minelittlepony.unicopia.Race;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
 class RaceArgument implements ArgumentType<Race> {
     static final Collection<String> EXAMPLES = Arrays.stream(Race.values())
@@ -18,6 +22,17 @@ class RaceArgument implements ArgumentType<Race> {
     @Override
     public Race parse(StringReader reader) throws CommandSyntaxException {
         return Race.fromName(reader.readUnquotedString(), Race.EARTH);
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
+        Arrays.stream(Race.values())
+            .filter(Race::isUsable)
+            .map(i -> i.name().toLowerCase())
+            .filter(i -> i.startsWith(builder.getRemaining().toLowerCase()))
+            .forEach(i -> builder.suggest(i.toLowerCase()));
+
+        return builder.buildFuture();
     }
 
     @Override
