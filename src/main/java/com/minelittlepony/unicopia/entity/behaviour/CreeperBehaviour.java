085c9dac2f1f691822782ca015a86778a06350cb
Sollace <sollacea@gmail.com>
2020-09-24 20:03:05 +0200

    Only trigger the creeper's action when not flying

--- src/main/java/com/minelittlepony/unicopia/entity/behaviour/CreeperBehaviour.java
+++ src/main/java/com/minelittlepony/unicopia/entity/behaviour/CreeperBehaviour.java
@@ -3,17 +3,24 @@ package com.minelittlepony.unicopia.entity.behaviour;
 import com.minelittlepony.unicopia.ability.magic.Caster;
 import com.minelittlepony.unicopia.ability.magic.Spell;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class CreeperBehaviour extends EntityBehaviour<CreeperEntity> {
     @Override
     public void update(Caster<?> source, CreeperEntity entity, Spell spell) {
-        if (source.getEntity().isSneaking()) {
+        if (isSneakingOnGround(source)) {
             entity.setFuseSpeed(1);
         } else {
             entity.setFuseSpeed(-1);
             entity.setTarget(null);
             entity.getVisibilityCache().clear();
         }
+    }
+
+    protected boolean isSneakingOnGround(Caster<?> source) {
+        Entity e = source.getEntity();
+        return e.isSneaking() && (e.isOnGround() || !(e instanceof PlayerEntity && ((PlayerEntity)e).abilities.flying));
     }
 }
