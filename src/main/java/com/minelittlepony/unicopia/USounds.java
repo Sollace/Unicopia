2f455ae6a75020457690b5558d70da421ee11500
Sollace <sollacea@gmail.com>
2020-09-24 14:49:13 +0200

    Clean up some sounds we no longer use

--- src/main/java/com/minelittlepony/unicopia/USounds.java
+++ src/main/java/com/minelittlepony/unicopia/USounds.java
@@ -9,11 +9,7 @@ public interface USounds {
     SoundEvent WING_FLAP = register("wing_flap");
     SoundEvent WIND_RUSH = register("wind_rush");
 
-    SoundEvent INSECT = register("insect");
     SoundEvent CHANGELING_BUZZ = register("changeling_buzz");
-
-    SoundEvent SLIME_ADVANCE = register("slime_advance");
-    SoundEvent SLIME_RETRACT = register("slime_retract");
 
     SoundEvent RECORD_CRUSADE = register("record.crusade");
     SoundEvent RECORD_PET = register("record.pet");
