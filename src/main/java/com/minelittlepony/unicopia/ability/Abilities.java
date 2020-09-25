cc0d661ae74880a36f57a1b4a6d90da459ee39f8
Sollace <sollacea@gmail.com>
2020-09-25 12:50:23 +0200

    Adjust the slots used for each ability

--- src/main/java/com/minelittlepony/unicopia/ability/Abilities.java
+++ src/main/java/com/minelittlepony/unicopia/ability/Abilities.java
@@ -19,15 +19,15 @@ public interface Abilities {
     Ability<?> TELEPORT = register(new UnicornTeleportAbility(), "teleport", AbilitySlot.SECONDARY);
 
     // earth / alicorn
-    Ability<?> STOMP = register(new EarthPonyStompAbility(), "stomp", AbilitySlot.PRIMARY);
     Ability<?> GROW = register(new EarthPonyGrowAbility(), "grow", AbilitySlot.SECONDARY);
+    Ability<?> STOMP = register(new EarthPonyStompAbility(), "stomp", AbilitySlot.TERTIARY);
 
     // pegasus / bat / alicorn / changeling
     Ability<?> CARRY = register(new CarryAbility(), "carry", AbilitySlot.PASSIVE);
 
     // changeling
-    Ability<?> DISGUISE = register(new ChangelingDisguiseAbility(), "disguise", AbilitySlot.PRIMARY);
-    Ability<?> FEED = register(new ChangelingFeedAbility(), "feed", AbilitySlot.SECONDARY);
+    Ability<?> DISGUISE = register(new ChangelingDisguiseAbility(), "disguise", AbilitySlot.SECONDARY);
+    Ability<?> FEED = register(new ChangelingFeedAbility(), "feed", AbilitySlot.TERTIARY);
 
     static <T extends Ability<?>> T register(T power, String name, AbilitySlot slot) {
         Identifier id = new Identifier("unicopia", name);
