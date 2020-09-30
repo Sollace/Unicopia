0059e92c2eed7b039a57208a93ed4b1b24cc131e
Sollace <sollacea@gmail.com>
2020-09-30 20:53:23 +0200

    Fixed attack goals, and fixed disguises hurting themselves

--- src/main/java/com/minelittlepony/unicopia/entity/behaviour/MobBehaviour.java
+++ src/main/java/com/minelittlepony/unicopia/entity/behaviour/MobBehaviour.java
@@ -8,11 +8,27 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 
 public class MobBehaviour extends EntityBehaviour<MobEntity> {
+
+    private MobEntity dummy;
+
     @Override
     public void update(Pony player, MobEntity entity, DisguiseSpell spell) {
 
         if (player.sneakingChanged() && isSneakingOnGround(player)) {
-            entity.tryAttack(RayTraceHelper.findEntity(player.getEntity(), 6, 1, e -> e instanceof LivingEntity).orElse(entity));
+
+            LivingEntity target = RayTraceHelper.<LivingEntity>findEntity(player.getEntity(), 6, 1,
+                    e -> e instanceof LivingEntity && e != entity && e != player.getOwner())
+                    .orElseGet(() -> getDummy(entity));
+
+            entity.tryAttack(target);
+            target.setAttacker(player.getOwner());
         }
+    }
+
+    private MobEntity getDummy(MobEntity entity) {
+        if (dummy == null) {
+            dummy = (MobEntity)entity.getType().create(entity.world);
+        }
+        return dummy;
     }
 }
