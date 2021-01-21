23f71c368cd3657fc924ba2433b01dbc0d192c2a
Sollace <sollacea@gmail.com>
2021-01-21 16:07:49 +0200

    Fix command auto-complete not functioning in multiplayer

--- src/main/java/com/minelittlepony/unicopia/command/Commands.java
+++ src/main/java/com/minelittlepony/unicopia/command/Commands.java
@@ -2,10 +2,13 @@ package com.minelittlepony.unicopia.command;
 
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.command.argument.ArgumentTypes;
+import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
 import net.minecraft.server.MinecraftServer;
 
 public class Commands {
     public static void bootstrap() {
+        ArgumentTypes.register("unicopia:race", RaceArgument.class, new ConstantArgumentSerializer<>(RaceArgument::new));
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             SpeciesCommand.register(dispatcher);
             RacelistCommand.register(dispatcher);
