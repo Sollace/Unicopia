c188b9c7cb9dac646dc4c38ece1aa1d6617fb405
Sollace <sollacea@gmail.com>
2021-03-06 12:08:14 +0200

    Siphoning spell should stay angry for as long as there are entities still inside it

--- src/main/java/com/minelittlepony/unicopia/ability/magic/spell/SiphoningSpell.java
+++ src/main/java/com/minelittlepony/unicopia/ability/magic/spell/SiphoningSpell.java
@@ -89,11 +89,9 @@ public class SiphoningSpell extends AbstractPlacedSpell {
                     setDead();
                 } else {
                     e.damage(damage, e.getHealth() / 4);
-                }
-                if (maxHealthGain <= 0) {
                     ticksUpset = 100;
+                    setDirty();
                 }
-                setDirty();
             } else {
                 e.heal((float)Math.min(0.5F * (1 + source.getLevel().get()), maxHealthGain * 0.6));
                 ParticleUtils.spawnParticle(new FollowingParticleEffect(UParticles.HEALTH_DRAIN, e, 0.2F), e.world, e.getPos(), Vec3d.ZERO);
