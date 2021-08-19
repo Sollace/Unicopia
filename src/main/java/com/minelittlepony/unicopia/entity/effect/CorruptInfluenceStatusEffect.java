7931a0b973d034af827e6ad4fa55445b7c696cb8
Sollace <sollacea@gmail.com>
2021-08-19 16:32:38 +0200

    Nerf the corrupt influence damage boost slightly

--- src/main/java/com/minelittlepony/unicopia/entity/effect/CorruptInfluenceStatusEffect.java
+++ src/main/java/com/minelittlepony/unicopia/entity/effect/CorruptInfluenceStatusEffect.java
@@ -19,7 +19,7 @@ import net.minecraft.world.WorldEvents;
 public class CorruptInfluenceStatusEffect extends StatusEffect {
     CorruptInfluenceStatusEffect(int color) {
         super(StatusEffectType.NEUTRAL, color);
-        addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, "6D706448-6A60-4F59-BE8A-C23A6DD2C7A9", 120, EntityAttributeModifier.Operation.ADDITION);
+        addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, "6D706448-6A60-4F59-BE8A-C23A6DD2C7A9", 15, EntityAttributeModifier.Operation.ADDITION);
         addAttributeModifier(EntityAttributes.GENERIC_ATTACK_SPEED, "6D706448-6A60-4F59-BE8A-C23A6DD2C7A9", 10, EntityAttributeModifier.Operation.ADDITION);
     }
 
