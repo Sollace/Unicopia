d84a6070461a3cd3f6b675b2820a77309b4dc51d
Sollace <sollacea@gmail.com>
2021-12-31 15:47:29 +0200

    Fixed frosted obsidian turning into water when mined

--- src/main/java/com/minelittlepony/unicopia/block/FrostedObsidianBlock.java
+++ src/main/java/com/minelittlepony/unicopia/block/FrostedObsidianBlock.java
@@ -1,15 +1,29 @@
 package com.minelittlepony.unicopia.block;
 
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FrostedIceBlock;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class FrostedObsidianBlock extends FrostedIceBlock {
     public FrostedObsidianBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
+        player.incrementStat(Stats.MINED.getOrCreateStat(this));
+        player.addExhaustion(0.005f);
+        Block.dropStacks(state, world, pos, blockEntity, player, stack);
     }
 
     @Override
