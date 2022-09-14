d38005c5bf8e5137509dc309acf4108dfd8fd70c
Sollace <sollacea@gmail.com>
2022-09-14 11:54:18 +0200

    Remove unused SpellContainer code

--- src/main/java/com/minelittlepony/unicopia/ability/magic/SpellContainer.java
+++ src/main/java/com/minelittlepony/unicopia/ability/magic/SpellContainer.java
@@ -10,41 +10,6 @@ import org.jetbrains.annotations.Nullable;
 import com.minelittlepony.unicopia.ability.magic.spell.Spell;
 
 public interface SpellContainer {
-    SpellContainer EMPTY = new SpellContainer() {
-        @Override
-        public <T extends Spell> Optional<T> get(SpellPredicate<T> type, boolean update) {
-            return Optional.empty();
-        }
-
-        @Override
-        public void put(Spell effect) { }
-
-        @Override
-        public boolean clear() {
-            return false;
-        }
-
-        @Override
-        public boolean removeIf(Predicate<Spell> effect, boolean update) {
-            return false;
-        }
-
-        @Override
-        public boolean forEach(Function<Spell, Operation> action, boolean update) {
-            return false;
-        }
-
-        @Override
-        public boolean contains(UUID id) {
-            return false;
-        }
-
-        @Override
-        public Stream<Spell> stream(boolean update) {
-            return Stream.empty();
-        }
-    };
-
     /**
      * Checks if a spell with the given uuid is present.
      */
@@ -94,51 +59,6 @@ public interface SpellContainer {
      * Removes all effects currently active in this slot.
      */
     boolean clear();
-
-    interface Delegate extends SpellContainer {
-
-        SpellContainer delegate();
-
-        @Override
-        default boolean contains(UUID id) {
-            return delegate().contains(id);
-        }
-
-        @Override
-        default boolean contains(@Nullable SpellPredicate<?> type) {
-            return delegate().contains(type);
-        }
-
-        @Override
-        default <T extends Spell> Optional<T> get(@Nullable SpellPredicate<T> type, boolean update) {
-            return delegate().get(type, update);
-        }
-
-        @Override
-        default void put(@Nullable Spell effect) {
-            delegate().put(effect);
-        }
-
-        @Override
-        default boolean removeIf(Predicate<Spell> effect, boolean update) {
-            return delegate().removeIf(effect, update);
-        }
-
-        @Override
-        default boolean forEach(Function<Spell, Operation> action, boolean update) {
-            return delegate().forEach(action, update);
-        }
-
-        @Override
-        default boolean clear() {
-            return delegate().clear();
-        }
-
-        @Override
-        default Stream<Spell> stream(boolean update) {
-            return delegate().stream(update);
-        }
-    }
 
     public enum Operation {
         SKIP,
