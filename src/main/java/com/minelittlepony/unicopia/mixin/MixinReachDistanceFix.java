fb0e8705359570bd14df5478ee92c2e7168fa280
Sollace <sollacea@gmail.com>
2022-09-18 22:31:31 +0200

    Fix Mixins even harder

--- src/main/java/com/minelittlepony/unicopia/mixin/MixinReachDistanceFix.java
+++ src/main/java/com/minelittlepony/unicopia/mixin/MixinReachDistanceFix.java
@@ -5,7 +5,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import com.minelittlepony.unicopia.entity.player.Pony;
 
+import net.minecraft.network.packet.c2s.play.*;
 import net.minecraft.server.network.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 @Mixin(value = {
         ServerPlayerInteractionManager.class,
@@ -13,10 +16,12 @@ import net.minecraft.server.network.*;
 })
 abstract class MixinReachDistanceFix {
     @Redirect(
-            method = {
-                    "processBlockBreakingAction",
-                    "onPlayerInteractBlock",
-                    "onPlayerInteractEntity"
+            target = {
+                    @Desc(owner = ServerPlayerInteractionManager.class, value = "processBlockBreakingAction", args = {
+                            BlockPos.class, PlayerActionC2SPacket.Action.class, Direction.class, int.class, int.class
+                    }),
+                    @Desc(owner = ServerPlayNetworkHandler.class, value = "onPlayerInteractBlock", args = { PlayerInteractBlockC2SPacket.class }),
+                    @Desc(owner = ServerPlayNetworkHandler.class, value = "onPlayerInteractEntity", args = { PlayerInteractEntityC2SPacket.class })
             },
             at = @At(
                 value = "FIELD",
