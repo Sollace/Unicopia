66cbf1ff2b3009ba50f65339065f235d755f05c4
Sollace <sollacea@gmail.com>
2022-09-26 21:07:14 +0200

    Document each slot type

--- src/main/java/com/minelittlepony/unicopia/ability/AbilitySlot.java
+++ src/main/java/com/minelittlepony/unicopia/ability/AbilitySlot.java
@@ -2,9 +2,21 @@ package com.minelittlepony.unicopia.ability;
 
 public enum AbilitySlot {
     NONE,
+    /**
+     * The primary ability. Corresponds to the largest ring in the HUD
+     */
     PRIMARY,
+    /**
+     * THe secondary ability. Corresponds to the top small ring in the HUD
+     */
     SECONDARY,
+    /**
+     * The tertiary ability. Corresponds to the bottom small ring in the HUD.
+     */
     TERTIARY,
+    /**
+     * The passive primary ability. Appears in place of the primary ability whilst sneaking.
+     */
     PASSIVE;
 
     public boolean isPassive() {
