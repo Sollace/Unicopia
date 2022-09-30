d720a37cd9e68a9df44088b59e75426bc0a06808
Sollace <sollacea@gmail.com>
2022-09-30 14:52:26 +0200

    Added creative tabs for each of the different races

--- src/main/java/com/minelittlepony/unicopia/item/UItemGroups.java
+++ src/main/java/com/minelittlepony/unicopia/item/UItemGroups.java
@@ -1,7 +1,8 @@
 package com.minelittlepony.unicopia.item;
 
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
 
+import com.minelittlepony.unicopia.UTags;
 import com.minelittlepony.unicopia.Unicopia;
 import com.minelittlepony.unicopia.item.toxin.Toxic;
 import com.minelittlepony.unicopia.item.toxin.ToxicHolder;
@@ -9,6 +10,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.tag.TagKey;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.registry.Registry;
@@ -26,9 +28,27 @@ public interface UItemGroups {
     ItemGroup HORSE_FEED = FabricItemGroupBuilder.create(Unicopia.id("horsefeed")).appendItems(list -> {
         list.addAll(Registry.ITEM.stream()
                 .map(Item::getDefaultStack)
-                .filter(item -> ((ToxicHolder)item.getItem()).getToxic(item) != Toxic.EMPTY)
-                .collect(Collectors.toList()));
+                .filter(stack -> ((ToxicHolder)stack.getItem()).getToxic(stack) != Toxic.EMPTY)
+                .toList());
     }).icon(UItems.ZAP_APPLE::getDefaultStack).build();
+
+    ItemGroup EARTH_PONY_ITEMS = forTag("earth_pony", UItems.APPLE_PIE::getDefaultStack);
+    ItemGroup UNICORN_ITEMS = forTag("unicorn", UItems.SPELLBOOK::getDefaultStack);
+    ItemGroup PEGASUS_ITEMS = forTag("pegasus", UItems.PEGASUS_FEATHER::getDefaultStack);
+    ItemGroup BAT_PONY_ITEMS = forTag("bat_pony", UItems.SUNGLASSES::getDefaultStack);
+    ItemGroup CHANGELING_ITEMS = forTag("changeling", Items.SCULK_SHRIEKER::getDefaultStack);
+
+    static ItemGroup forTag(String name, Supplier<ItemStack> icon) {
+        TagKey<Item> key = UTags.item("groups/" + name);
+        return FabricItemGroupBuilder.create(Unicopia.id(name)).appendItems(list -> {
+            list.addAll(Registry.ITEM.getEntryList(key)
+                    .stream()
+                    .flatMap(named -> named.stream())
+                    .map(entry -> entry.value())
+                    .map(Item::getDefaultStack)
+                    .toList());
+        }).icon(icon).build();
+    }
 
     static void bootstrap() {}
 }
