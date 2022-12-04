980355851bb3ee897cae58fdc7d7aa9d4aab5bef
Sollace <sollacea@gmail.com>
2022-12-04 15:37:52 +0000

    Fix crash with quilt. Fixes #84

--- src/main/java/com/minelittlepony/unicopia/Debug.java
+++ src/main/java/com/minelittlepony/unicopia/Debug.java
@@ -1,16 +1,33 @@
 package com.minelittlepony.unicopia;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.impl.util.Arguments;
 
 public final class Debug {
     public static final boolean DEBUG_SPELLBOOK_CHAPTERS;
     public static final boolean DEBUG_COMMANDS;
 
     static {
-        Arguments args = new Arguments();
-        args.parse(FabricLoader.getInstance().getLaunchArguments(true));
+        Map<String, String> args = parseArguments(FabricLoader.getInstance().getLaunchArguments(true));
         DEBUG_SPELLBOOK_CHAPTERS = "true".equalsIgnoreCase(args.getOrDefault("unicopia.debug.spellbookChapters", "false"));
         DEBUG_COMMANDS = "true".equalsIgnoreCase(args.getOrDefault("unicopia.debug.commands", "false"));
+    }
+
+    static Map<String, String> parseArguments(String[] args) {
+        Map<String, String> arguments = new HashMap<>();
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            String next = i < args.length - 1 ? args[i + 1] : null;
+
+            if (arg.startsWith("--") && next != null && !next.startsWith("--")) {
+                arguments.put(arg, next);
+                i++;
+            }
+        }
+
+        return arguments;
     }
 }
