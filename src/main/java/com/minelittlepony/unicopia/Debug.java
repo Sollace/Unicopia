9421587f9dc0ae11eb1d1883f736bca57096e8dd
Sollace <sollacea@gmail.com>
2022-12-04 19:13:57 +0000

    Switch to using JVM args for debugging

--- src/main/java/com/minelittlepony/unicopia/Debug.java
+++ src/main/java/com/minelittlepony/unicopia/Debug.java
@@ -1,33 +1,6 @@
 package com.minelittlepony.unicopia;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public final class Debug {
-    public static final boolean DEBUG_SPELLBOOK_CHAPTERS;
-    public static final boolean DEBUG_COMMANDS;
-
-    static {
-        Map<String, String> args = parseArguments(FabricLoader.getInstance().getLaunchArguments(true));
-        DEBUG_SPELLBOOK_CHAPTERS = "true".equalsIgnoreCase(args.getOrDefault("--unicopia.debug.spellbookChapters", "false"));
-        DEBUG_COMMANDS = "true".equalsIgnoreCase(args.getOrDefault("--unicopia.debug.commands", "false"));
-    }
-
-    static Map<String, String> parseArguments(String[] args) {
-        Map<String, String> arguments = new HashMap<>();
-
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            String next = i < args.length - 1 ? args[i + 1] : null;
-
-            if (arg.startsWith("--") && next != null && !next.startsWith("--")) {
-                arguments.put(arg, next);
-                i++;
-            }
-        }
-
-        return arguments;
-    }
+public interface Debug {
+    boolean DEBUG_SPELLBOOK_CHAPTERS = Boolean.getBoolean("unicopia.debug.spellbookChapters");
+    boolean DEBUG_COMMANDS = Boolean.getBoolean("unicopia.debug.commands");
 }
