f55dff6b87dc9043bb559df64bcff3935f55fd1b
Sollace <sollacea@gmail.com>
2022-12-24 21:12:54 +0100

    Properly implement death messages for pegasi

--- src/main/java/com/minelittlepony/unicopia/mixin/MixinDamageSource.java
+++ src/main/java/com/minelittlepony/unicopia/mixin/MixinDamageSource.java
@@ -1,8 +1,10 @@
 package com.minelittlepony.unicopia.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.minelittlepony.unicopia.entity.Equine;
@@ -10,30 +12,50 @@ import com.minelittlepony.unicopia.entity.player.Pony;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.*;
 import net.minecraft.text.Text;
 
 @Mixin(DamageSource.class)
 abstract class MixinDamageSource {
     @Inject(method = "getDeathMessage", at = @At("RETURN"), cancellable = true)
     private void onGetDeathMessage(LivingEntity entity, CallbackInfoReturnable<Text> info) {
-        Equine.of(entity).map(Equine::getAttacker).ifPresent(attacker -> {
-            DamageSource self = (DamageSource)(Object)this;
+        final DamageSource self = (DamageSource)(Object)this;
 
-            Entity prime = entity.getPrimeAdversary();
-            if (prime != null && !attacker.isOwnedBy(prime)) {
-                info.setReturnValue(Text.translatable("death.attack.generic.and_also", info.getReturnValue(), attacker.asEntity().getDisplayName()));
-                return;
-            }
+        if (self.isFromFalling()) {
+            info.setReturnValue(new DamageSource(self.name + ".pegasus").getDeathMessage(entity));
+        } else {
+            Equine.of(entity).map(Equine::getAttacker).ifPresent(attacker -> {
+                Entity prime = entity.getPrimeAdversary();
+                if (prime != null && !attacker.isOwnedBy(prime)) {
+                    info.setReturnValue(Text.translatable("death.attack.generic.and_also", info.getReturnValue(), attacker.asEntity().getDisplayName()));
+                    return;
+                }
 
-            info.setReturnValue(Text.translatable("death.attack." + self.getName() + ".player", entity.getDisplayName(), attacker.asEntity().getDisplayName()));
-        });
+                info.setReturnValue(Text.translatable("death.attack." + self.getName() + ".player", entity.getDisplayName(), attacker.asEntity().getDisplayName()));
+            });
 
-        DamageSource self = (DamageSource)(Object)this;
 
-        Pony.of(entity).filter(e -> e.getSpecies().canFly()).ifPresent(pony -> {
-            if (pony.getPhysics().isFlying()) {
-                info.setReturnValue(Text.translatable("death.attack.generic.whilst_flying", info.getReturnValue()));
+            Pony.of(entity).filter(e -> e.getSpecies().canFly()).ifPresent(pony -> {
+                if (pony.getPhysics().isFlying()) {
+                    info.setReturnValue(Text.translatable("death.attack.generic.whilst_flying", info.getReturnValue()));
+                }
+            });
+        }
+    }
+}
+
+@Mixin(DamageTracker.class)
+abstract class MixinDamageTracker {
+    @Shadow
+    private @Final LivingEntity entity;
+    @Nullable
+    private String fallDeathSuffix;
+
+    @Inject(method = "setFallDeathSuffix", at = @At("RETURN"))
+    private void onSetFallDeathSuffix(CallbackInfo info) {
+        Pony.of(entity).ifPresent(pony -> {
+            if (pony.getSpecies().canFly()) {
+                fallDeathSuffix = (fallDeathSuffix == null ? "" : fallDeathSuffix + ".") + "pegasus";
             }
         });
     }
---
+++ src/main/java/com/minelittlepony/unicopia/mixin/client/MixinTranslationStorage.java
@@ -0,0 +1,33 @@
+package com.minelittlepony.unicopia.mixin.client;
+
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.resource.language.TranslationStorage;
+
+@Mixin(TranslationStorage.class)
+abstract class MixinTranslationStorage {
+    @Shadow
+    private @Final Map<String, String> translations;
+
+    @Inject(method = "get", at = @At("HEAD"), cancellable = true)
+    public void onGet(String key, CallbackInfoReturnable<String> info) {
+        if (key != null && key.contains(".pegasus") && !translations.containsKey(key)) {
+            info.setReturnValue(translations.getOrDefault(key.replace(".pegasus", ""), key));
+        }
+    }
+
+    @Inject(method = "hasTranslation", at = @At("RETURN"), cancellable = true)
+    public void onHasTranslation(String key, CallbackInfoReturnable<Boolean> info) {
+        if (key != null
+                && key.contains(".pegasus")
+                && !info.getReturnValueZ()
+                && translations.containsKey(key.replace(".pegasus", ""))) {
+            info.setReturnValue(true);
+        }
+    }
+}
