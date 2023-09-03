80cb31222b2ea604d84d61cbb90b7c7a3da36a71
Sollace <sollacea@gmail.com>
2023-09-03 22:13:36 +0100

    Change the horse feed creative tab into a "Foraging" tab and make more blocks foragable

--- src/main/java/com/minelittlepony/unicopia/item/group/UItemGroups.java
+++ src/main/java/com/minelittlepony/unicopia/item/group/UItemGroups.java
@@ -4,23 +4,15 @@ import java.util.stream.Stream;
 
 import com.minelittlepony.unicopia.item.ChameleonItem;
 import com.minelittlepony.unicopia.item.UItems;
-import com.minelittlepony.unicopia.item.toxin.Toxic;
-import com.minelittlepony.unicopia.item.toxin.ToxicHolder;
-
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
 
 public interface UItemGroups {
-    RegistryKey<ItemGroup> ALL_ITEMS = ItemGroupRegistry.createDynamic("items", UItems.EMPTY_JAR::getDefaultStack, () -> {
+    RegistryKey<ItemGroup> ALL_ITEMS = ItemGroupRegistry.createDynamic("items", UItems.CRYSTAL_HEART::getDefaultStack, () -> {
         return Stream.concat(Stream.of(Items.APPLE), ItemGroupRegistry.ITEMS.stream()
                 .filter(item -> !(item instanceof ChameleonItem) || ((ChameleonItem)item).isFullyDisguised()));
     });
-    RegistryKey<ItemGroup> HORSE_FEED = ItemGroupRegistry.createDynamic("horsefeed", UItems.ZAP_APPLE::getDefaultStack, () -> {
-        return Registries.ITEM.stream()
-                .filter(item -> ((ToxicHolder)item).getToxic(item.getDefaultStack()) != Toxic.EMPTY);
-    });
-
+    RegistryKey<ItemGroup> FORAGING_ITEMS = ItemGroupRegistry.createGroupFromTag("foraging", Items.HAY_BLOCK::getDefaultStack);
     RegistryKey<ItemGroup> EARTH_PONY_ITEMS = ItemGroupRegistry.createGroupFromTag("earth_pony", UItems.EARTH_BADGE::getDefaultStack);
     RegistryKey<ItemGroup> UNICORN_ITEMS = ItemGroupRegistry.createGroupFromTag("unicorn", UItems.UNICORN_BADGE::getDefaultStack);
     RegistryKey<ItemGroup> PEGASUS_ITEMS = ItemGroupRegistry.createGroupFromTag("pegasus", UItems.PEGASUS_BADGE::getDefaultStack);
