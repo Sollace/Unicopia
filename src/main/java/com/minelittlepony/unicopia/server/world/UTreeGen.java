e1f4d775809f1559246b6543eb3120c11a904f36
Sollace <sollacea@gmail.com>
2024-02-01 13:26:57 +0000

    Adjust tree heights to remove the possibility of trees generating absurdly tall

--- src/main/java/com/minelittlepony/unicopia/server/world/UTreeGen.java
+++ src/main/java/com/minelittlepony/unicopia/server/world/UTreeGen.java
@@ -24,7 +24,7 @@ import net.minecraft.world.gen.trunk.UpwardsBranchingTrunkPlacer;
 
 public interface UTreeGen {
     Tree ZAP_APPLE_TREE = Tree.Builder.create(Unicopia.id("zap_apple_tree"), new UpwardsBranchingTrunkPlacer(
-                    7, 2, 3,
+                    5, 3, 0,
                     UniformIntProvider.create(3, 6),
                     0.3f,
                     UniformIntProvider.create(1, 3),
@@ -47,7 +47,7 @@ public interface UTreeGen {
     Tree SWEET_APPLE_TREE = createAppleTree("sweet_apple", UBlocks.SWEET_APPLE_LEAVES, 3);
     Tree SOUR_APPLE_TREE = createAppleTree("sour_apple", UBlocks.SOUR_APPLE_LEAVES, 5);
     Tree GOLDEN_APPLE_TREE = Tree.Builder.create(Unicopia.id("golden_oak_tree"),
-            new StraightTrunkPlacer(6, 7, 3),
+            new StraightTrunkPlacer(6, 1, 3),
             new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), 3)
         )
             .configure(TreeFeatureConfig.Builder::forceDirt)
@@ -90,7 +90,7 @@ public interface UTreeGen {
 
     static Tree createAppleTree(String name, Block leaves, int preferredDensity) {
         return Tree.Builder.create(Unicopia.id(name + "_tree"),
-                new StraightTrunkPlacer(4, 6, 2),
+                new StraightTrunkPlacer(4, 3, 2),
                 new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), 3)
             )
                 .configure(TreeFeatureConfig.Builder::forceDirt)
