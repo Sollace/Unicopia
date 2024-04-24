cda4e5b0b614fdeba90c6ea156a79fc74cb8e7be
Sollace <sollacea@gmail.com>
2024-04-24 20:00:44 +0100

    Change ad-astra oxygen check to use the current location instead of the entity

--- src/main/java/com/minelittlepony/unicopia/compat/ad_astra/OxygenApi.java
+++ src/main/java/com/minelittlepony/unicopia/compat/ad_astra/OxygenApi.java
@@ -2,10 +2,11 @@ package com.minelittlepony.unicopia.compat.ad_astra;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public interface OxygenApi {
-    AtomicReference<OxygenApi> API = new AtomicReference<>(entity -> false);
+    AtomicReference<OxygenApi> API = new AtomicReference<>((world, pos) -> false);
 
-    boolean hasOxygen(Entity entity);
+    boolean hasOxygen(World world, BlockPos pos);
 }
