7560df438cd582503c6c662c00b08949e38058ab
Sollace <sollacea@gmail.com>
2024-05-20 16:02:52 +0100

    Move server-affecting mixins to their own package

--- src/main/java/com/minelittlepony/unicopia/mixin/server/MixinServerWorld.java
+++ src/main/java/com/minelittlepony/unicopia/mixin/server/MixinServerWorld.java
@@ -1,6 +1,5 @@
-package com.minelittlepony.unicopia.mixin;
+package com.minelittlepony.unicopia.mixin.server;
 
-import java.util.List;
 import java.util.function.BooleanSupplier;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,16 +7,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.minelittlepony.unicopia.server.world.BlockDestructionManager;
 import com.minelittlepony.unicopia.server.world.NocturnalSleepManager;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.server.world.SleepManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
@@ -55,13 +51,5 @@ abstract class MixinServerWorld extends World implements StructureWorldAccess, N
     ))
     public void beforeWakeup(BooleanSupplier shouldKeepTicking, CallbackInfo info) {
         getNocturnalSleepManager().skipTime();
-    }
-}
-
-@Mixin(SleepManager.class)
-abstract class MixinSleepManager {
-    @ModifyVariable(method = "update(Ljava/util/List;)Z", at = @At("HEAD"))
-    public List<ServerPlayerEntity> modifyPlayers(List<ServerPlayerEntity> players) {
-        return players.size() <= 0 ? players : ((NocturnalSleepManager.Source)players.get(0).getWorld()).getNocturnalSleepManager().filterPlayers(players);
     }
 }
