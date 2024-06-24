6eb2464d78ad94a29fcd940a99cc3a3838c5989a
Sollace <sollacea@gmail.com>
2024-06-24 01:25:28 +0100

    Revert mixinextras usage

--- src/main/java/com/minelittlepony/unicopia/mixin/MixinGuardianTargetPredicate.java
+++ src/main/java/com/minelittlepony/unicopia/mixin/MixinGuardianTargetPredicate.java
@@ -5,7 +5,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import com.minelittlepony.unicopia.entity.effect.SeaponyGraceStatusEffect;
 
 import net.minecraft.entity.LivingEntity;
@@ -16,8 +18,10 @@ abstract class MixinGuardianTargetPredicate {
     @Shadow
     private @Final GuardianEntity owner;
 
-    @ModifyReturnValue(method = "test", at = @At("RETURN"))
-    private boolean unicopia_excludeSeaponysGrace(boolean result, @Nullable LivingEntity target) {
-        return result && SeaponyGraceStatusEffect.hasIre(target, owner);
+    @Inject(method = "test", at = @At("HEAD"), cancellable = true)
+    private void unicopia_excludeSeaponysGrace(@Nullable LivingEntity target, CallbackInfoReturnable<Boolean> info) {
+        if (!SeaponyGraceStatusEffect.hasIre(target, owner)) {
+            info.setReturnValue(false);
+        }
     }
 }
