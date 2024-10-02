5a900e8601c29659ae5c20f40c1f42b1073ad1a0
Sollace <sollacea@gmail.com>
2024-10-02 23:32:27 +0100

    Remove unneccessary injection

--- src/main/java/com/minelittlepony/unicopia/client/BatEyesApplicator.java
+++ src/main/java/com/minelittlepony/unicopia/client/BatEyesApplicator.java
@@ -3,9 +3,9 @@ package com.minelittlepony.unicopia.client;
 import com.minelittlepony.unicopia.EquinePredicates;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 
 public class BatEyesApplicator {
 
@@ -15,11 +15,17 @@ public class BatEyesApplicator {
 
     private final MinecraftClient client = MinecraftClient.getInstance();
 
+    public float getWorldBrightness(float initial, LivingEntity entity, float tickDelta) {
+        if (!EquinePredicates.PLAYER_BAT.test(entity)) {
+            return initial;
+        }
+        return 0.6F;
+    }
+
     public void enable() {
-        if (client.world != null) {
-            PlayerEntity player = client.player;
-            if (!player.hasStatusEffect(StatusEffects.NIGHT_VISION) && EquinePredicates.PLAYER_BAT.test(player)) {
-                player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1, 1, false, false));
+        if (client.world != null && client.player != null) {
+            if (!client.player.hasStatusEffect(StatusEffects.NIGHT_VISION) && EquinePredicates.PLAYER_BAT.test(client.player)) {
+                client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, -1, 1, false, false));
                 batEyesApplied = true;
             }
         }
