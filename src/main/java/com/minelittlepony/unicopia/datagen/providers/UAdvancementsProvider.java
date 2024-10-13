39597a0a1c94f3716914562b24a51e6aa3702f58
Sollace <sollacea@gmail.com>
2024-10-13 18:18:50 +0100

    Change criterion check to an OR for tribe joins

--- src/main/java/com/minelittlepony/unicopia/datagen/providers/UAdvancementsProvider.java
+++ src/main/java/com/minelittlepony/unicopia/datagen/providers/UAdvancementsProvider.java
@@ -22,6 +22,7 @@ import net.minecraft.advancement.AdvancementCriterion;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementFrame;
 import net.minecraft.advancement.AdvancementRequirements;
+import net.minecraft.advancement.AdvancementRequirements.CriterionMerger;
 import net.minecraft.advancement.AdvancementRewards;
 import net.minecraft.advancement.criterion.ConsumeItemCriterion;
 import net.minecraft.advancement.criterion.Criteria;
@@ -74,6 +75,7 @@ public class UAdvancementsProvider extends FabricAdvancementProvider {
             for (Race r : extra) {
                 builder.criterion("be_" + r.getId().getPath(), UCriteria.PLAYER_CHANGE_RACE.create(new RaceChangeCriterion.Conditions(Optional.empty(), r)));
             }
+            builder.criteriaMerger(CriterionMerger.OR);
         }
 
         return builder.build(consumer, race.getId().getPath() + "_route");
