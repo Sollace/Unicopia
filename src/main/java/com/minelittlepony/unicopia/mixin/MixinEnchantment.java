37a1d52f8cdb8f2874e33d0679af79500f945a22
Sollace <sollacea@gmail.com>
2024-12-10 22:41:43 +0100

    Remove empty mixin

--- src/main/java/com/minelittlepony/unicopia/mixin/MixinEnchantment.java
+++
@@ -1,9 +0,0 @@
-package com.minelittlepony.unicopia.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.enchantment.Enchantment;
-
-@Mixin(Enchantment.class)
-abstract class MixinEnchantment {
-
-}
