8af4a0e08d48a5c9a54efcce4cb9dd770c0b32bf
Sollace <sollacea@gmail.com>
2025-10-03 00:00:01 +0100

    Specter improvements

--- src/main/java/com/minelittlepony/unicopia/client/render/entity/SpecterEntityRenderer.java
+++ src/main/java/com/minelittlepony/unicopia/client/render/entity/SpecterEntityRenderer.java
@@ -1,8 +1,21 @@
 package com.minelittlepony.unicopia.client.render.entity;
 
+import java.util.Set;
+
+import com.minelittlepony.unicopia.Unicopia;
+import com.minelittlepony.unicopia.client.render.RenderLayers;
 import com.minelittlepony.unicopia.entity.mob.SpecterEntity;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.ModelData;
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
@@ -10,8 +23,9 @@ import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.Direction;
 
 public class SpecterEntityRenderer extends BipedEntityRenderer<SpecterEntity, SpecterEntityRenderer.SpecterEntityModel> {
 
@@ -25,17 +39,50 @@ public class SpecterEntityRenderer extends BipedEntityRenderer<SpecterEntity, Sp
 
     @Override
     public Identifier getTexture(SpecterEntity entity) {
-        return PlayerScreenHandler.BLOCK_ATLAS_TEXTURE;
+        return Unicopia.id("textures/entity/specter.png");
+    }
+
+    @Override
+    protected void setupTransforms(SpecterEntity entity, MatrixStack matrices, float animationProgress, float bodyYaw, float tickDelta, float scale) {
+        super.setupTransforms(entity, matrices, animationProgress, 0, tickDelta, scale);
+        Camera cam = MinecraftClient.getInstance().gameRenderer.getCamera();
+        matrices.multiply(cam.getRotation());
     }
 
     static class SpecterEntityModel extends BipedEntityModel<SpecterEntity> {
+        private final ModelPart part;
+        private int alpha;
+
         public SpecterEntityModel(ModelPart root) {
-            super(root);
+            super(root, RenderLayer::getEntityTranslucent);
+            this.part = createModelData().createModel();
+        }
+
+        static TexturedModelData createModelData() {
+            ModelData data = new ModelData();
+            ModelPartData root = data.getRoot();
+            root.addChild("eyes", ModelPartBuilder.create().uv(0, 0).cuboid(-4, -6, -4, 8, 8, 8, Set.of(Direction.NORTH)), ModelTransform.NONE);
+            return TexturedModelData.of(data, 64, 32);
+        }
+
+        @Override
+        public void animateModel(SpecterEntity entity, float f, float g, float h) {
+            super.animateModel(entity, f, g, h);
+            double distance = entity.squaredDistanceTo(MinecraftClient.getInstance().gameRenderer.getCamera().getPos());
+            alpha = distance <= 400 ? 0 : ColorHelper.channelFromFloat((float)Math.clamp(((distance - 400D) / 600D), 0, 1));
         }
 
         @Override
         public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int color) {
-            // noop
+            if (alpha <= 0) {
+                return;
+            }
+            vertices = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers().getBuffer(RenderLayers.getEyes(Unicopia.id("textures/entity/specter.png")));
+            matrices.push();
+            float scale = 1 + alpha / 80F;
+            matrices.scale(scale, scale, scale);
+            part.render(matrices, vertices, LightmapTextureManager.MAX_LIGHT_COORDINATE, overlay, ColorHelper.Argb.withAlpha(alpha, color));
+            matrices.pop();
         }
     }
 }
