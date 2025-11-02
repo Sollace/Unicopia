ba089a5d4b49bf0f2f376d171978360a0abb51ee
Sollace <sollacea@gmail.com>
2025-11-02 20:26:55 +0000

    Remove deprecated method

--- src/main/java/com/minelittlepony/unicopia/ability/magic/spell/trait/Trait.java
+++ src/main/java/com/minelittlepony/unicopia/ability/magic/spell/trait/Trait.java
@@ -163,16 +163,11 @@ public enum Trait implements CommandArgumentEnum<Trait> {
                 .flatMap(Optional::stream);
     }
 
-    @Deprecated
-    public static Optional<Trait> fromId(Identifier id) {
-        return Optional.ofNullable(IDS.get(id));
-    }
-
     public static Optional<Trait> of(String name) {
         @SuppressWarnings("deprecation")
         Trait trait = NAME_CODEC.byId(name);
         if (trait == null) {
-            return Optional.ofNullable(Identifier.tryParse(name)).flatMap(Trait::fromId);
+            return Optional.ofNullable(Identifier.tryParse(name)).map(IDS::get);
         }
         return Optional.ofNullable(trait);
     }
