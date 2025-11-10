dcc186930a5a6508040603548d3c61ecd64d269f
Sollace <sollacea@gmail.com>
2025-11-10 19:01:25 +0000

    Fixed using the ability on an entity you're already holding not releasing it

--- src/main/java/com/minelittlepony/unicopia/entity/player/LevitatedItemsInventory.java
+++ src/main/java/com/minelittlepony/unicopia/entity/player/LevitatedItemsInventory.java
@@ -74,28 +74,29 @@ public class LevitatedItemsInventory implements Copyable<LevitatedItemsInventory
     public boolean addPassenger(Entity passenger) {
         double yOffset = 0.1;
         if (passenger.getRootVehicle() instanceof LevitatingItemEntity root) {
-            if (root.getMaster() == player.asEntity() && root.getPassengerList().size() == 1 && root.getPassengerList().get(0) == passenger) {
-                player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1);
-                return true;
-            }
+            boolean isOnlyPassenger = root.getPassengerList().size() == 1 && root.getPassengerList().get(0) == passenger;
             passenger.stopRiding();
             passenger.setPosition(root.getPos());
             yOffset = 0;
             if (root.getPassengerList().isEmpty()) {
                 root.kill();
             }
+            if (root.getMaster() == player.asEntity() && isOnlyPassenger) {
+                player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1);
+                return true;
+            }
         }
         LevitatingItemEntity entity = new LevitatingItemEntity(nextSlot++, player.asEntity(), ItemStack.EMPTY);
-        onEntitySpawned(entity);
         entity.setPosition(passenger.getPos());
         entity.setHoldingPosition(passenger.getPos().add(0, yOffset, 0));
-        player.asWorld().spawnEntity(entity);
         if (passenger instanceof ItemEntity i) {
             entity.setStack(i.getStack());
             i.discard();
             return addStack(i.getStack());
         } else {
+            onEntitySpawned(entity);
             passenger.startRiding(entity, true);
+            player.asWorld().spawnEntity(entity);
         }
 
         player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1);
@@ -104,6 +105,9 @@ public class LevitatedItemsInventory implements Copyable<LevitatedItemsInventory
 
     public void dropEverything() {
         List<LevitatingItemEntity> copy = new ArrayList<>(stacks.values());
+        if (!copy.isEmpty()) {
+            player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1);
+        }
         stacks.clear();
         nextSlot = 0;
         copy.forEach(stack -> stack.kill());
