2db0c68efb28d1ab383b073f68be41e85806f082
Sollace <sollacea@gmail.com>
2026-03-12 15:45:10 +0000

    Document the moon phase when stage changes

--- src/main/java/com/minelittlepony/unicopia/server/world/ZapAppleStageStore.java
+++ src/main/java/com/minelittlepony/unicopia/server/world/ZapAppleStageStore.java
@@ -77,7 +77,7 @@ public class ZapAppleStageStore extends PersistentState implements Tickable {
                 && MeteorlogicalUtil.isBetween(prevSkyAngle, MeteorlogicalUtil.SUNSET, MeteorlogicalUtil.MIDNIGHT)
             )) {
                 stageChanged = true;
-                if (lastStage != Stage.HIBERNATING || world.getMoonPhase() == 0) {
+                if (lastStage != Stage.HIBERNATING || world.getMoonPhase() == MeteorlogicalUtil.FULL_MOON) {
                     lastStage = lastStage.getNext();
                     playedMoonEffect = false;
                     markDirty();
