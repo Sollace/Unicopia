5f294602aa10ecd42b2fd16f28ca26cf94c82485
Sollace <sollacea@gmail.com>
2026-04-02 23:18:55 +0100

    Fix compile issue (wut)

--- src/main/java/com/minelittlepony/unicopia/util/Untyped.java
+++ src/main/java/com/minelittlepony/unicopia/util/Untyped.java
@@ -2,7 +2,7 @@ package com.minelittlepony.unicopia.util;
 
 public interface Untyped {
     @SuppressWarnings("unchecked")
-    static <K, T extends K> T cast(K t) {
+    static <K, T> T cast(K t) {
         return (T)t;
     }
 }
