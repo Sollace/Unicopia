1e456c36367182ba32ecae2bebdb92f209ecf8af
Sollace <sollacea@gmail.com>
2026-08-07 01:13:22 +0100

    Implement the consumer interface. #628
    While PacketPayloadAcceptor has an accept(T) method, it doesn't match the interface due to differing signatures

--- src/main/java/com/minelittlepony/unicopia/mixin/forgified/MixinPacketAndPayloadAcceptor.java
+++ src/main/java/com/minelittlepony/unicopia/mixin/forgified/MixinPacketAndPayloadAcceptor.java
@@ -2,14 +2,22 @@ package com.minelittlepony.unicopia.mixin.forgified;
 
 import java.util.function.Consumer;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.network.listener.ClientCommonPacketListener;
 import net.minecraft.network.packet.Packet;
 
 @Pseudo
 @Mixin(targets = "net.neoforged.neoforge.network.bundle.PacketAndPayloadAcceptor")
-interface MixinPacketAndPayloadAcceptor<L extends ClientCommonPacketListener> extends Consumer<Packet<? extends L>> {
+abstract class MixinPacketAndPayloadAcceptor<L extends ClientCommonPacketListener> implements Consumer<Packet<? super L>> {
+    @Shadow(remap = false)
+    private @Final Consumer<Packet<? super L>> consumer;
 
+    @Override
+    public void accept(Packet<? super L> packet) {
+        consumer.accept(packet);
+    }
 }
